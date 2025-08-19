b22cb3d57e91112c44f34b21e279c9382d0d3d37
Ali Bouzar <ali.bouzar@gmail.com>
2025-08-19 11:26:13 +0200

    Add Eureka Discovery server and an API gateway service

--- eureka-server/src/main/java/com/cqsr/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/cqsr/EurekaServerApplication.java
@@ -2,14 +2,14 @@ package com.cqsr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class StoreQueryApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreQueryApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
